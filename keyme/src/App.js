05033aa33624f0a37d76f6335b1807bd74349da1
amber phillips <amber.ranae2@gmail.com>
2018-06-19 19:38:53 -0500

    image,json

--- keyme/src/App.js
+++ keyme/src/App.js
@@ -2,100 +2,45 @@ import React, { Component } from "react";
 import Click from "./components/Click";
 import wrapper from "./components/wrapper";
 import Title from "./components/Title";
-///import friends from "./friends.json";
+import click from "./click.json";
 import "./App.css";
 
 ///const IMAGES = ["cat", "cat", "dog", "dog", "horse", "horse", "pig",
   ///"pig", "snake", "snake", "fish", "fish"];
 
-class MemoryGame extends Component {
-  constructor(props) {
-    super(props);
-
-    // You can simplify your state a lot
-    this.state = {
-      cards: shuffleArray(IMAGES.slice()),
-      selected: [], // indexes which have been selected
-      correct: [] // indexes which have been guessed correctly
+  class App extends Component {
+    // Setting this.state.friends to the friends json array
+    state = {
+      click
     };
-  }
-
-  // Don't need a componentWillMount
-
-  onCardClick(clickedIndex) {
-    const { selected, cards, correct } = this.state;
-
-    if (selected.length === 0) { // selecting a first card
-      this.setState({ selected: [clickedIndex] })
-    } else if (selected.length === 1) { // they're selecting a second card
-      if (cards[selected[0]] === cards[clickedIndex]) {
-        // It's a match :)
-        // Add selected cards to `correct` and reset selection
-        this.setState({
-            correct: correct.concat([selected[0], clickedIndex]),
-            selected: []
-          });
-        } else {
-          // It's not a match :(
-          // Select it for now, and reset selection in a bit
-          this.setState({ selected: [selected[0], clickedIndex] });
-          setTimeout(() => {
-            this.setState({ selected: [] })
-          }, 1500);
-        }
-      }
-      // Otherwise they already have 2 selected and we don't wanna do anything
-    }
   
+    removeFriend = id => {
+      // Filter this.state.friends for friends with an id not equal to the id being removed
+      const friends = this.state.friends.filter(friend => friend.id !== id);
+      // Set this.state.friends equal to the new friends array
+      this.setState({ friends });
+    };
+  
+    // Map over this.state.friends and render a FriendCard component for each friend object
     render() {
-      const { correct, selected, cards } = this.state;
       return (
-        <div>
-          <h1>Memory Game</h1>
-          <div className="mui-panel wrapper">
-            {cards.map((image, i) => (
-              <MemoryCard
-                key={i}
-                image={image}
-                isCorrect={correct.includes(i)}
-                isSelected={selected.includes(i)}
-                onSelect={() => this.onCardClick(i)}
-              />
-            ))}
-          </div>
-          </div>
-    );
+        <wrapper>
+          <Title>Friends List</Title>
+          {this.state.friends.map(friend => (
+            <Click
+              removeFriend={this.removeFriend}
+              id={friend.id}
+              key={friend.id}
+              name={friend.name}
+              image={friend.image}
+              occupation={friend.occupation}
+              location={friend.location}
+            />
+          ))}
+        </wrapper>
+      );
+    }
   }
-}
-
-// Extracted into it's own component
-const MemoryCard = ({ image, isSelected, isCorrect, onSelect }) => (
-  <div
-    className="modal mui-panel"
-    onClick={() => {
-      // You can only select a card that's not already correct and
-      // isn't currently selected
-      if (!isCorrect && !isSelected) {
-        onSelect();
-      }
-    }}
-  >
-    <img
-      style={{ visibility: (isCorrect || isSelected) ? 'visible' : 'hidden' }}
-      src={"./" + image + ".jpg"}
-      srcSet={"./" + image + "_lrg.jpg 1000w"}
-      alt={image}
-    />
-  </div>
-);
-
-// Probably in a different file
-function shuffleArray(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    let j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-  return array;
-}
-
-export default MemoryGame;       
+  
+  export default App;
+  
---
+++ keyme/src/components/Click/click.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./click.css";
+
+const Click = props => (
+  <div className="card">
+    <div className="img-container">
+      <img alt={props.name} src={props.image} />
+    </div>
+    <div className="content">
+      <ul>
+        <li>
+          <strong>Name:</strong> {props.name}
+        </li>
+        <li>
+          <strong>Occupation:</strong> {props.occupation}
+        </li>
+        <li>
+          <strong>Location:</strong> {props.location}
+        </li>
+      </ul>
+    </div>
+      𝘅
+  </div>
+);
+
+export default Click;
