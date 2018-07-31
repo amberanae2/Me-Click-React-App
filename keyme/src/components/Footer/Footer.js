f1642ada9ff94e7a41d36e966bc93b26c6eac374
amber phillips <amber.ranae2@gmail.com>
2018-07-30 19:29:46 -0500

    updated game

---
+++ keyme/src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => (
+  <footer className="footer">
+    <div className="bottom">
+    Built With ReactJS
+    </div>
+  </footer>
+);
+
+export default Footer;
